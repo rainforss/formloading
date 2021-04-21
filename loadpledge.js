25613cf76309507b5ac95fa89849352db22509b8
rainforss <shengyan@ualberta.ca>
2021-04-21 10:01:56 -0600

    Update loadpledge.js

--- loadpledge.js
+++ loadpledge.js
@@ -26,6 +26,10 @@ modalButton.addEventListener("click", checkAndClose);
 let checkboxClicked = false;
 let checkboxTarget;
 let formsWrapper;
+let checkboxes = document.querySelectorAll(
+  `input[name="be63b8b8-4f20-eb11-a813-000d3af3d354"]`
+);
+checkboxes.forEach((c) => c.addEventListener("click", checkboxPrompt));
 
 function checkboxPrompt(e) {
   checkboxClicked = true;
